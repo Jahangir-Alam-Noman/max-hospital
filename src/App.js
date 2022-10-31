0f19f52652fe63f79bc0418a49db02eb1b6cb482
Jahangir-Alam-Noman <jahangir2km@gmail.com>
2022-11-01 02:22:28 +0600

    max-hospital Home added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Hello This is testing Part for Noman</h2>
+     <p>
+       lorem ispam is the Simplt text formatted text which is very important for us 
+     </p>
+    </div>
+  );
+}
+
+
+export default App;
